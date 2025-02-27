da396440e6cb88bdb50e783225c7f3dddc4786eb
Neslihan Avsar <neslihan.avsar@symbionet.io>
2025-02-27 09:47:15 +0100

    Created new page for a users own events

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import LoginPage from '../components/pages/LoginPage/LoginPage';
 import PrivateRoute from './PrivateRoute';
 import HomePage from '../components/pages/HomePage';
@@ -8,50 +8,52 @@ import EditEventPage from '../components/pages/EditEventPage';
 import UserTable from '../components/pages/UserPage/UserTable';
 import UserPage from '../components/pages/UserPage/UserPage';
 import authorities from '../config/Authorities';
+import MyEventsPage from "../components/pages/MyEventsPage";
 
 /**
  * Router component renders a route switch with all available pages
  */
 
 const Router = () => {
-  //const { checkRole } = useContext(ActiveUserContext);
+    //const { checkRole } = useContext(ActiveUserContext);
 
-  /** navigate to different "home"-locations depending on Role the user have */
+    /** navigate to different "home"-locations depending on Role the user have */
 
-  return (
-    <Routes>
-      <Route path={'/'} element={<HomePage />} />
-      <Route path={'/login'} element={<LoginPage />} />
-      <Route path={'/home'} element={<HomePageLoggedIn />} />
-      <Route path={'/create-event'} element={<CreateEventPage />} />
-      <Route path={'/edit-event'} element={<EditEventPage />} />
+    return (
+        <Routes>
+            <Route path={'/'} element={<HomePage/>}/>
+            <Route path={'/login'} element={<LoginPage/>}/>
+            <Route path={'/home'} element={<HomePageLoggedIn/>}/>
+            <Route path={'/create-event'} element={<CreateEventPage/>}/>
+            <Route path={'/edit-event'} element={<EditEventPage/>}/>
+            <Route path={'/events'} element={<MyEventsPage/>}/>
 
-      <Route
-        path={'/users'}
-        element={<PrivateRoute requiredAuths={[]} element={<UserTable />} />}
-      />
-      <Route
-        path='/useredit'
-        element={
-          <PrivateRoute
-            requiredAuths={[authorities.USER_DEACTIVATE, authorities.USER_CREATE]}
-            element={<UserPage />}
-          ></PrivateRoute>
-        }
-      />
-      <Route
-        path='/useredit/:userId'
-        element={
-          <PrivateRoute
-            requiredAuths={[authorities.USER_READ]}
-            element={<UserPage />}
-          ></PrivateRoute>
-        }
-      />
+            <Route
+                path={'/users'}
+                element={<PrivateRoute requiredAuths={[]} element={<UserTable/>}/>}
+            />
+            <Route
+                path='/useredit'
+                element={
+                    <PrivateRoute
+                        requiredAuths={[authorities.USER_DEACTIVATE, authorities.USER_CREATE]}
+                        element={<UserPage/>}
+                    ></PrivateRoute>
+                }
+            />
+            <Route
+                path='/useredit/:userId'
+                element={
+                    <PrivateRoute
+                        requiredAuths={[authorities.USER_READ]}
+                        element={<UserPage/>}
+                    ></PrivateRoute>
+                }
+            />
 
-      <Route path='*' element={<div>Not Found</div>} />
-    </Routes>
-  );
+            <Route path='*' element={<div>Not Found</div>}/>
+        </Routes>
+    );
 };
 
 export default Router;
